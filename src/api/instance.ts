d5a8e0af2f965117cb0df5d59ff55c45b95a62a5
Andrii Gadar <gadar.andre@gmail.com>
2024-02-19 04:11:29 +0200

    deployment settings

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL!;
-
 export const instance = axios.create({
-  baseURL: REACT_APP_API_BASE_URL,
+  baseURL: 'https://housing-hub-back.onrender.com',
 });
